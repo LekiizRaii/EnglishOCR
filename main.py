8b84744faefb102a3f9e3bd4d942da35e5c511fc
vuthn <vu.truongcompsci@hcmut.edu.vn>
2023-11-19 15:04:43 +0700

    feat: delete Vu's character segmentation method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from character_segmentation import CharacterSegmentation
 from utils import *
 
-path = "../iam_words/words/r06/r06-126/r06-126-09-05.png"
+path = "../iam_words/words/r06/r06-126/r06-126-09-04.png"
+# path = "../iam_words/words/a01/a01-000u/a01-000u-01-02.png"
 module = CharacterSegmentation(path)
 module.run()
