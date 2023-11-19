1d94e16fe2ab1f29ef08d3a7ec1f734fd4231587
vuthn <vu.truongcompsci@hcmut.edu.vn>
2023-11-19 15:14:54 +0700

    feat: refine our repo

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from character_segmentation import CharacterSegmentation
-from utils import *
+from character_segmentation import character_segmentation
 
-path = "../iam_words/words/r06/r06-126/r06-126-09-04.png"
-# path = "../iam_words/words/a01/a01-000u/a01-000u-01-02.png"
-module = CharacterSegmentation(path)
+path = "./iam_words/words/c03/c03-016d/c03-016d-02-05.png"
+returnPath = "result/char"
+module = character_segmentation.CharacterSegmentation2(path, returnPath)
 module.run()
