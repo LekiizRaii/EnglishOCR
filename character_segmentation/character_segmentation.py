d299c2f28389e518b7fd6aee7e5be8d749d46573
Mai Phuong Nha <phuongnha922017@gmail.com>
2023-11-20 17:05:17 +0700

    segmentation modified

--- character_segmentation/character_segmentation.py
+++ character_segmentation/character_segmentation.py
@@ -16,8 +16,14 @@ class CharacterSegmentation2:
         gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
         _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
+        #Expand the image to the left and right with 2 background col
+        h = thresh.shape[0]
+        w = thresh.shape[1]
+        expand = np.zeros((h, w+2), np.uint8)
+        expand[0:h, 1 : w + 1] = thresh[0:h, 0:w]
+
         #Thin the word
-        thinned = cv2.ximgproc.thinning(thresh, thinningType=cv2.ximgproc.THINNING_GUOHALL)
+        thinned = cv2.ximgproc.thinning(expand, thinningType=cv2.ximgproc.THINNING_GUOHALL)
 
         #Binarize
         pixelArr = asarray(thinned)
@@ -36,15 +42,14 @@ class CharacterSegmentation2:
         plt.show()
 
         # Merge PSCs into segmentation columns (SCs)
-        groupedPSCs = []  
+        groupedPSCs = [[0]]  
         for i in range(len(PSCs)):
             if i == 0:
-                groupedPSCs.append([PSCs[i]])
+                continue
+            if PSCs[i] - PSCs[i-1] <= self.threshold:
+                groupedPSCs[-1].append(PSCs[i])
             else:
-                if PSCs[i] - PSCs[i-1] <= self.threshold:
-                    groupedPSCs[-1].append(PSCs[i])
-                else:
-                    groupedPSCs.append([PSCs[i]])
+                groupedPSCs.append([PSCs[i]])
         SCs = []
         for i in range(len(groupedPSCs)):
             SCs.append(np.around(np.mean(groupedPSCs[i])).astype(int))
@@ -56,15 +61,9 @@ class CharacterSegmentation2:
     def segmentCharacters(self, SCs):
         s=0
         charImgList = []
-        for i in range(len(SCs)):
-            if i==0:
-                s=SCs[i]
-                charImg = self.img[0:,0:s]
-            else:
-                charImg = self.img[0:,s:SCs[i]]
-                s = SCs[i]
+        for i in range(len(SCs) - 1):
+            charImg = self.img[0:, SCs[i] : SCs[i+1]]
             charImgList.append(charImg)
-        charImgList.append(self.img[0:,SCs[-1]:,])
         return charImgList
 
     def segment(self, charImgList):
@@ -74,7 +73,6 @@ class CharacterSegmentation2:
             charPath = self.resultPath + str(counter) +".jpeg"
             print(charPath)
             cv2.imwrite(charPath, charImg)
-    
             counter = counter + 1
        
     def run(self):
@@ -82,3 +80,5 @@ class CharacterSegmentation2:
         SCs = self.findSegCol(thinned, pixelArr)
         charImgList = self.segmentCharacters(SCs)
         self.segment(charImgList) 
+
+    
